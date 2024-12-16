7183f782b1ff0a85913c3305a863a8bc6f06b468
Olajide Olaniyan <jidd32@gmail.com>
2024-12-16 00:28:58 +0000

    update with cloudflare plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,51 @@
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-cloudflare';
+
+
+// export default {
+// 	kit: {
+// 		// default options are shown
+// 		adapter: adapter({
+// 			// if true, will create a Netlify Edge Function rather
+// 			// than using standard Node-based functions
+// 			edge: false,
+
+// 			// if true, will split your app into multiple functions
+// 			// instead of creating a single one for the entire app.
+// 			// if `edge` is true, this option cannot be used
+// 			split: false
+// 		})
+// 	}
+// };
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// See below for an explanation of these options
+// 			routes: {
+// 				include: ['/*'],
+// 				exclude: ['<all>']
+// 			},
+// 			platformProxy: {
+// 				configPath: 'wrangler.toml',
+// 				environment: undefined,
+// 				experimentalJsonConfig: false,
+// 				persist: false
+// 			}
+// 		})
+// 	}
+// };
 
 export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
+  kit: {
+    adapter: adapter({
+      // options
+      pages: 'build', // Output directory for Cloudflare
+      assets: 'build', // Directory for static assets
+      fallback: null // Set to "index.html" for SPA behavior
+    }),
+    prerender: {
+      default: true // Prerender all pages
+    }
+  }
 };
