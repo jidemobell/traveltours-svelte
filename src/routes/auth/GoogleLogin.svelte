ea2d374a305c837f884e5f74ce57d1de08bbf59a
Olajide Olaniyan <jidd32@gmail.com>
2025-05-24 16:35:21 +0100

    Remove @neondatabase/serverless dependency

--- src/routes/auth/GoogleLogin.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import LoginWithGoogle from './LoginWithGoogle.svelte';
-</script>
-
-
-<main style="padding: 2rem; margin-top: 7rem; margin-bottom: 7rem; border: 2px red solid; max-width: 600px; margin-left: auto; margin-right: auto; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); border-radius: 8px; background: white;">
-    <LoginWithGoogle />
-</main>
--- src/routes/auth/logout.js
+++
@@ -1,5 +0,0 @@
-// import { serialize } from 'cookie';
-
-export async function LOGOUT({cookies}) {
-  cookies.delete("token")
-}
